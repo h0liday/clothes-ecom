82354ff149faee39cc4a9d6021edce72bc2c921b
h0l1day <yarechek.arabichek@gmail.com>
2019-07-17 15:28:59 +0200

    Added firebase and sign in

--- src/App.js
+++ src/App.js
@@ -5,11 +5,33 @@ import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state= {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+   this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState({ currentUser: user})
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
   return (
     <>
-    <Header />
+    <Header currentUser={this.state.currentUser}/>
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path="/shop" component={ShopPage} />
@@ -17,6 +39,6 @@ function App() {
     </Switch>
     </>
   );
-}
+}}
 
 export default App;
