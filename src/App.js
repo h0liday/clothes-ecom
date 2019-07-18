05e0082761199d16f387ee7c63cee7fe0b63cb10
h0l1day <yarechek.arabichek@gmail.com>
2019-07-18 13:11:00 +0200

    Added sign up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -18,9 +18,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-   this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({ currentUser: user})
-      console.log(user)
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+
+            }
+          })
+        })
+      }
+     this.setState({ currentUser: userAuth});
     })
   }
 
@@ -33,6 +44,7 @@ class App extends React.Component {
     <>
     <Header currentUser={this.state.currentUser}/>
     <Switch>
+      {console.log(this.state)}
       <Route exact path='/' component={HomePage} />
       <Route path="/shop" component={ShopPage} />
       <Route path="/signin" component={SignInAndSignUpPage} />
--- src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../signin/sign-in.component';
 import './sign-in-and-sign-up.styles.scss';
+import SignUp from '../sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
