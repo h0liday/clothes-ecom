1870348716b493ec6ad28ab91e767d20324006b8
h0l1day <yarechek.arabichek@gmail.com>
2019-08-04 11:47:19 +0200

    Small changes

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config = {
+    apiKey: "AIzaSyBwQw88FF4tz0ibjVqVcdkEQrKj_A3QO1Q",
+    authDomain: "clothes-db.firebaseapp.com",
+    databaseURL: "https://clothes-db.firebaseio.com",
+    projectId: "clothes-db",
+    storageBucket: "",
+    messagingSenderId: "439754535338",
+    appId: "1:439754535338:web:7d7570f4099bd77e"
+  };
+
+  export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (err) {
+        console.log('error creating user', err)
+      }
+    }
+    return userRef;
+  }
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
