4df5b3f2b4d75af08d867bb1b0462ef578e17acc
h0l1day <yarechek.arabichek@gmail.com>
2019-08-28 14:01:37 +0200

    Starting to refactoring thunk to sagas

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart} from '../../redux/shop/shop.actions';
 
 
 
@@ -15,8 +15,8 @@ class ShopPage extends React.Component  {
     
        
     componentDidMount() {
-        const { fetchCollectionsStartAsync } = this.props;
-        fetchCollectionsStartAsync();
+        const { fetchCollectionsStart} = this.props;
+        fetchCollectionsStart();
     }
 
     render () {
@@ -33,7 +33,7 @@ class ShopPage extends React.Component  {
 
 
     const mapDispatchToProps = dispatch => ({
-       fetchCollectionsStartAsync: ( ) => dispatch(fetchCollectionsStartAsync())
+       fetchCollectionsStart: ( ) => dispatch(fetchCollectionsStart())
     })
 
 
---
+++ src/redux/shop/shop.saga.js
@@ -0,0 +1,34 @@
+import { takeLatest, call, put } from 'redux-saga/effects';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
+
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionAsync() {
+    yield console.log('I am an fire')
+
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (err) {
+        yield put(fetchCollectionsFailure(err.message))
+    }
+   
+  
+   // collectionRef
+   // .get()
+   //     .then(snapshot => {
+   //       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+   //       dispatch(fetchCollectionsSuccess(collectionsMap));
+   //     })
+   //     .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+
+}
+
+export function* fetchCollectionStart() {
+    yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionAsync )
+}
